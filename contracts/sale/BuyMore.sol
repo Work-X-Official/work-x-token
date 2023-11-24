6876fa978001110334dbdd70be3e270689e5184d
Daniel de Witte <Artimunor@users.noreply.github.com>
2023-11-24 13:46:47 +0100

    added NatSpec for BuyMore.sol

--- contracts/sale/BuyMore.sol
+++ contracts/sale/BuyMore.sol
@@ -11,6 +11,14 @@ contract BuyMore {
     mapping(address => uint256) public investments;
     mapping(string => IERC20Metadata) public acceptedTokens;
 
+    event BoughtMore(address indexed sender, uint256 amount);
+
+    /**
+     * @dev Constructor referencing the target wallet and the accepted tokens
+     * @param _targetWallet the wallet the bought tokens will be sent to
+     * @param _tokenNames names of allowed tokens to spend
+     * @param _tokenAddresses addresses of allowed tokens to spend
+     **/
     constructor(address _targetWallet, string[] memory _tokenNames, address[] memory _tokenAddresses) {
         require(_tokenNames.length == _tokenAddresses.length, "BuyMore: Invalid accepted tokens length");
         targetWallet = _targetWallet;
@@ -19,14 +27,31 @@ contract BuyMore {
         }
     }
 
+    /**
+     * @notice Buy more tokens
+     * @dev Wraps _buyMore
+     * @param tokenName name of the token spent
+     * @param amount amount of tokens spent
+     **/
     function buyMore(string memory tokenName, uint256 amount) public {
         _buyMore(tokenName, amount);
     }
 
+    /**
+     * @dev Gets the total allocation of a user
+     * @param user to get allocation for
+     * @return total allocation of the user
+     **/
     function getTotalAllocation(address user) public view returns (uint256) {
         return investments[user];
     }
 
+    /**
+     * @notice _buyMore normalized decimals of the token (stablecoin have differences) and transfers the tokens to the target wallet
+     * @dev This function requires this contract to have been approved to spend the amount of tokens
+     * @param tokenName name of the token spent
+     * @param amount amount of tokens spent
+     **/
     function _buyMore(string memory tokenName, uint256 amount) private {
         require(acceptedTokens[tokenName] != IERC20Metadata(address(0)), "BuyMore: Invalid tokenName");
         require(amount > 0, "BuyMore: You can't invest 0 tokens");
@@ -37,10 +62,23 @@ contract BuyMore {
         investments[msg.sender] = investments[msg.sender] + normalizedAmount;
     }
 
+    /**
+     * @dev Normalizes the decimals of the token to the NORMALIZING_DECIMALS
+     * @param amount amount of tokens to normalize
+     * @param token token to get decimals from
+     * @return normalized amount
+     **/
     function _normalizeDecimals(uint256 amount, IERC20Metadata token) private view returns (uint256) {
         return amount * 10 ** (NORMALIZING_DECIMALS - uint256(token.decimals()));
     }
 
+    /**
+     * @dev Safely transfers tokens from the token contract to the target wallet
+     * @param token token to transfer
+     * @param from address to transfer from
+     * @param to address to transfer to
+     * @param value amount of tokens to transfer
+     **/
     function _safeTransferFrom(address token, address from, address to, uint256 value) internal {
         // bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
