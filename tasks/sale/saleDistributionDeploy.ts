1addcd18e432a7c6968406c534b6e23d456eb571
Daniel de Witte <Artimunor@users.noreply.github.com>
2023-12-04 13:27:04 +0400

    fix: private presale addresses

--- tasks/sale/saleDistributionDeploy.ts
+++ tasks/sale/saleDistributionDeploy.ts
@@ -3,14 +3,16 @@ import "@nomiclabs/hardhat-waffle";
 import { task } from "hardhat/config";
 import { WORK_TOKEN_ADDRESSES } from "../constants/workToken.constants";
 
-// example: yarn hardhat  sale:distribution:deploy --network sepolia
+// example: yarn hardhat sale:distribution:deploy --network sepolia
 
 task("sale:distribution:deploy").setAction(async function (_, hre) {
   // IMPORTANT! Make sure you have previously deployed the WorkToken contract, and added it's address to the constants
   const workToken = (await hre.ethers.getContractFactory("WorkToken")).attach(
     WORK_TOKEN_ADDRESSES[hre.network.name as keyof typeof WORK_TOKEN_ADDRESSES],
   );
-  const startTime = (await hre.ethers.provider.getBlock("latest")).timestamp + 80400;
+  const startTime = (await hre.ethers.provider.getBlock("latest")).timestamp + 60 * 30;
+  // const startTime = 1701770400; //
+  const startTimeDate = new Date(startTime * 1000);
   const accounts = await hre.ethers.getSigners();
   const distribution = await (
     await hre.ethers.getContractFactory("TokenDistribution")
@@ -19,7 +21,8 @@ task("sale:distribution:deploy").setAction(async function (_, hre) {
   console.log("╔══════════════════════════════════════════════════════════════════════");
   console.log("║ TokenDistribution deployed to:", distribution.address);
   console.log("║ On network: ", hre.network.name);
-  console.log("║");
+  console.log("║ The startTime is in seconds:", startTime);
+  console.log("║ The startTime in Date:", startTimeDate);
 
   const initRole = await distribution.grantRole(await distribution.INIT_ROLE(), accounts[0].address);
   const initGrantRole = await initRole.wait();
