ecf44567ce7e2cdec94eaf3d45ced03636076110
vantwoutmaarten <maarten@vantwout.net>
2024-01-22 12:02:05 +0100

    fix: remove Reward contract as a parent contract, and let the child contracts all implement these functions seperately, also adjusted the tests for RewardShares and RewardTokens to both test all the functions individually.

--- contracts/reward/RewardTokens.sol
+++ contracts/reward/RewardTokens.sol
@@ -1,15 +1,176 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.22;
 
-import "./Reward.sol";
+import "./../interface/IGenesisNft.sol";
+import "./../interface/IRewarder.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RewardTokens is Reward {
+error ClaimNotAllowed();
+
+contract RewardTokens is Ownable {
+     IGenesisNft public nft;
+    IERC20 public workToken;
+
+    address public rewardWrapper;
+
+    mapping(uint256 => uint256) public claimed;
+
+    uint256 private constant REWARD_MONTHS = 40;
+    uint256 private constant ONE_E18 = 10 ** 18;
+
+    mapping(uint256 => uint256) public monthClaimed;
+
+    /**
+     * @notice  The formula is: (40000 - sqrt(month * 40000000)) * 10 / 4 , but is multiplied by 10 ** 18 to get the amount in wei.
+     * Array with total reward amounts per month, filled with the formula above.
+     */
+    uint24[REWARD_MONTHS] private rewards = [
+        100000,
+        84189,
+        77639,
+        72614,
+        68377,
+        64645,
+        61270,
+        58167,
+        55279,
+        52566,
+        50000,
+        47560,
+        45228,
+        42991,
+        40839,
+        38763,
+        36754,
+        34808,
+        32918,
+        31080,
+        29289,
+        27543,
+        25838,
+        24171,
+        22540,
+        20943,
+        19377,
+        17842,
+        16334,
+        14853,
+        13397,
+        11966,
+        10557,
+        9170,
+        7805,
+        6459,
+        5132,
+        3823,
+        2532,
+        1258
+    ];
+
+    event Claimed(uint256 indexed nftId, address indexed claimer, uint256 amountClaimed);
     /**
      * @notice Initializes the RewardTokens contract with the given addresses.
      * @param _genesisNftAddress The address of the Genesis NFT contract.
      * @param _workTokenAddress The address of the WORK token contract.
      */
-    constructor(address _genesisNftAddress, address _workTokenAddress) Reward(_genesisNftAddress, _workTokenAddress) {}
+    constructor(address _genesisNftAddress, address _workTokenAddress) {
+        nft = IGenesisNft(_genesisNftAddress);
+        workToken = IERC20(_workTokenAddress);
+    }
+
+
+    /****
+     **** ONLY OWNER
+     ****/
+
+    /**
+     * @notice Rescue function for the contract owner to withdraw any ERC20 token from this contract.
+     * @dev A failsafe for any token stuck in this contract. Only callable by the contract owner.
+     * @param _tokenAddress Address of the ERC20 token contract.
+     * @param _amount Amount of the ERC20 token to withdraw.
+     **/
+    function withdrawTokens(address _tokenAddress, uint256 _amount) external payable onlyOwner {
+        IERC20(_tokenAddress).transfer(msg.sender, _amount);
+    }
+
+    /**
+     * @notice Approve a spender to spend an amount of WORK tokens from this contract, only callable by the contract owner.
+     * @param _spender Address of the spender.
+     * @param _amount Amount of the WORK token to approve.
+     * @return _success True if the operation was successful.
+     **/
+    function approve(address _spender, uint256 _amount) external onlyOwner returns (bool _success) {
+        _success = workToken.approve(_spender, _amount);
+    }
+
+    /****
+     **** EXTERNAL WRITE
+     ****/
+
+    /**
+     * @notice Claim the claimable reward of a nftId into the nft contract, can be done by the owner of the nftId.
+     * @param _nftId Id of the nft in which you want to claim the reward.
+     */
+    function claim(uint256 _nftId) external {
+        if (msg.sender != nft.ownerOf(_nftId) && msg.sender != rewardWrapper) {
+            revert ClaimNotAllowed();
+        }
+        uint256 currentMonth = nft.getCurrentMonth();
+
+        if (currentMonth == 0) {
+            return;
+        }
+
+        uint256 claimableNftId = getClaimable(_nftId);
+
+        if (claimableNftId > 0) {
+            claimed[_nftId] += claimableNftId;
+            monthClaimed[_nftId] = currentMonth;
+            nft.reward(_nftId, claimableNftId);
+            emit Claimed(_nftId, msg.sender, claimableNftId);
+        }
+        return;
+    }
+
+    /**
+     * @notice Set the reward wrapper address, only callable by the contract owner.
+     * @param _rewardWrapper Address of the reward wrapper contract.
+     */
+    function setRewardWrapper(address _rewardWrapper) external onlyOwner {
+        rewardWrapper = _rewardWrapper;
+    }
+
+    /****
+     **** PUBLIC VIEW
+     ****/
+
+    /**
+     * @notice Amount of tokens currently claimable, which is all rewards of a nftId that have not been claimed yet.
+     * @dev Calculates the reward for a nftId by summing the rewards of all previous months from month after last claim.
+     * @param _nftId Id of the nft for which you want to find the claimable amount.
+     * @return _claimable Amount of tokens currently claimable.
+     */
+    function getClaimable(uint256 _nftId) public view returns (uint256 _claimable) {
+        uint256 currentMonth = nft.getCurrentMonth();
+
+        for (uint256 i = monthClaimed[_nftId] + 1; i <= currentMonth; ++i) {
+            _claimable += getRewardNftIdMonth(_nftId, i);
+        }
+    }
+
+    /**
+     * @notice Get the total reward for a specific month according to the rewards array. In month 0 there is no reward and from month 1,
+     * the rewards are stored in the array.
+     * @param _month Month for which you want to find the total reward.
+     * @return _rewardTotalMonth Total reward in specific month.
+     */
+    function getRewardTotalMonth(uint256 _month) public view returns (uint256 _rewardTotalMonth) {
+        if (_month > REWARD_MONTHS || _month == 0) {
+            return 0;
+        }
+        _rewardTotalMonth = rewards[_month - 1] * ONE_E18;
+    }
 
     /**
      * @notice Calculates the reward of a nftId for a specific month based on the total rewards of this month and the minimum staked of the previous month.
@@ -20,7 +181,7 @@ contract RewardTokens is Reward {
     function getRewardNftIdMonth(
         uint256 _nftId,
         uint256 _month
-    ) public view override returns (uint256 _rewardNftIdMonth) {
+    ) public view returns (uint256 _rewardNftIdMonth) {
         if (_month == 0) {
             return 0;
         }
--- test/reward/RewardTokens.test.ts
+++ test/reward/RewardTokens.test.ts
@@ -14,6 +14,7 @@ import {
   mineStakeMonths,
   testGetRewardNftIdMonth,
   claimAndVerifyClaimed,
+  testMonthClaimed,
 } from "../util/reward.util";
 
 config();
@@ -58,6 +59,26 @@ describe("RewardTokens", () => {
     await distribution.setWalletClaimable([nftMinter1.address], [25000], [0], [0], [0]);
     await distribution.setWalletClaimable([nftMinter2.address], [50000], [0], [0], [0]);
     await distribution.setWalletClaimable([nftMinter3.address], [150000], [0], [0], [0]);
+  });
+
+  describe("Testing getRewardTotalMonth, total reward function on RewardBase", async () => {
+    it("getRewardTotalMonth is correct month 0, 0", async () => {
+      expect(await reward.getRewardTotalMonth(0)).to.equal(0);
+    });
+
+    it("getRewardersTotal is correct for month 1 till 40", async () => {
+      for (let i = 1; i <= 40; i++) {
+        expect(await reward.getRewardTotalMonth(i)).to.equal(amount(REWARDS[i - 1]));
+      }
+    });
+
+    it("getRewardTotalMonth is correct 0 for month 41, and several months after that", async () => {
+      expect(await reward.getRewardTotalMonth(41)).to.equal(0);
+      expect(await reward.getRewardTotalMonth(42)).to.equal(0);
+      expect(await reward.getRewardTotalMonth(50)).to.equal(0);
+      expect(await reward.getRewardTotalMonth(100)).to.equal(0);
+      expect(await reward.getRewardTotalMonth(amount(100000000000000))).to.equal(0);
+    });
   });
 
   describe("Testing reward per nft per month from tokens, getRewardNftIdMonth", async () => {
@@ -180,6 +201,39 @@ describe("RewardTokens", () => {
         await expect(reward.getRewardNftIdMonth(nftId1, 1)).to.be.reverted;
         await expect(reward.getRewardNftIdMonth(nftId1, 2)).to.be.reverted;
       });
+    });
+  });
+
+  describe("Testing approve function", async () => {
+    before(async () => {
+      const startTime = (await ethers.provider.getBlock("latest")).timestamp + 32;
+      ({
+        workToken,
+        distribution,
+        nft,
+        rewardTokens: reward,
+      } = await regenerateContracts(accounts, accounts[0].address, startTime));
+      await distribution.setWalletClaimable([nftMinter1.address], [25000], [0], [0], [0]);
+      const amountMint1 = 25000;
+      ({ nftId: nftId1 } = await mintNft(network, nft, workToken, nftMinter1, amountMint1, 0, 0, chainId));
+      await mineDays(22, network);
+      await mineDays(30, network);
+    });
+
+    it("Should revert when claim when nft has not enough allowance", async () => {
+      await expect(reward.connect(nftMinter1).claim(nftId1)).to.be.revertedWith("ERC20: insufficient allowance");
+    });
+
+    it("Should Revert when not owner tries to approve", async () => {
+      await expect(reward.connect(nftMinter2).approve(nft.address, amount(1000000))).to.be.revertedWith(
+        "Ownable: caller is not the owner",
+      );
+    });
+
+    it("Allowance is set correctly with approve", async () => {
+      await reward.approve(nft.address, amount(1000000));
+      const allowance = await workToken.allowance(reward.address, nft.address);
+      expect(allowance).to.equal(amount(1000000));
     });
   });
 
@@ -312,6 +366,59 @@ describe("RewardTokens", () => {
         expect(await reward.getClaimable(nftId2)).to.equal(sumRewards2);
         expect(await reward.getClaimable(nftId3)).to.equal(sumRewards3);
       });
+    });
+  });
+
+  describe("Simple claim, error and events", async () => {
+    before(async () => {
+      const startTime = (await ethers.provider.getBlock("latest")).timestamp + 40;
+      ({
+        workToken,
+        distribution,
+        nft,
+        rewardTokens: reward,
+      } = await regenerateContracts(accounts, accounts[0].address, startTime));
+      await distribution.setWalletClaimable([nftMinter1.address], [25000], [0], [0], [0]);
+      await reward.approve(nft.address, amount(1000000));
+    });
+
+    it("Cannot claim when the token does not exists", async () => {
+      await expect(reward.connect(nftMinter1).claim("0")).to.be.revertedWith("ERC721: invalid token ID");
+    });
+
+    it("Mint nft 1 and go to startime", async () => {
+      const amountMint1 = 25000;
+      ({ nftId: nftId1 } = await mintNft(network, nft, workToken, nftMinter1, amountMint1, 0, 0, chainId));
+      expect(await nft.ownerOf(nftId1)).to.be.equal(nftMinter1.address);
+
+      await mineDays(22, network);
+    });
+
+    it("Should revert when you are not the owner", async () => {
+      await expect(reward.connect(nftMinter2).claim(nftId1)).to.be.revertedWith("ClaimNotAllowed");
+    });
+
+    it("Right after mint you cannot claim yet, so nothing is transfered", async () => {
+      await expect(reward.connect(nftMinter1).claim(nftId1)).to.not.emit(nft, "Transfer");
+      await expect(reward.connect(nftMinter1).claim(nftId1)).to.not.emit(reward, "Claimed");
+    });
+
+    it("During first month (on day 20), nothing to claim", async () => {
+      await mineDays(20, network);
+      await expect(reward.connect(nftMinter1).claim(nftId1)).to.not.emit(nft, "Transfer");
+    });
+
+    it("On day 30, after 1 month, the getCurrentMonth correctly returns 1", async () => {
+      await mineDays(10, network);
+      const currentMonth = await nft.getCurrentMonth();
+      expect(currentMonth).to.equal(1);
+    });
+
+    it("Claim emits a Claimed event and increases the staked of the nft", async () => {
+      const stakedBeforeNft1 = Number(ethers.utils.formatEther((await nft.getStaked(nftId1, 1))[0]));
+      await expect(reward.connect(nftMinter1).claim(nftId1)).to.emit(reward, "Claimed");
+      const stakedAfterNft1 = Number(ethers.utils.formatEther((await nft.getStaked(nftId1, 1))[0]));
+      expect(stakedAfterNft1).to.be.gt(stakedBeforeNft1);
     });
   });
 
@@ -550,6 +657,78 @@ describe("RewardTokens", () => {
         await testGetRewardNftIdMonth(reward, nft, nftId2, 25000 + claimedNftId2, 100000 + claimedNftId2, 10);
         await testGetRewardNftIdMonth(reward, nft, nftId2, 25000 + claimedNftId2, 100000 + claimedNftId2, 11);
       });
+    });
+  });
+
+  describe("Test monthClaimed, it should keep track of the month in which nft last claimed reward", async () => {
+    before(async () => {
+      const startTime = (await ethers.provider.getBlock("latest")).timestamp + 32;
+      ({
+        workToken,
+        distribution,
+        nft,
+        rewardTokens: reward,
+      } = await regenerateContracts(accounts, accounts[0].address, startTime));
+      await distribution.setWalletClaimable([nftMinter1.address], [25000], [0], [0], [0]);
+      await distribution.setWalletClaimable([nftMinter2.address], [50000], [0], [0], [0]);
+      await reward.approve(nft.address, amount(1400000));
+    });
+
+    it("Mint nft 1,2 and go to startime", async () => {
+      const amountMint1 = 25000;
+      ({ nftId: nftId1 } = await mintNft(network, nft, workToken, nftMinter1, amountMint1, 0, 0, chainId));
+      expect(await nft.ownerOf(nftId1)).to.be.equal(nftMinter1.address);
+      const amountMint2 = 50000;
+      ({ nftId: nftId2 } = await mintNft(network, nft, workToken, nftMinter2, amountMint2, 0, 0, chainId));
+      expect(await nft.ownerOf(nftId2)).to.be.equal(nftMinter2.address);
+
+      await mineDays(22, network);
+    });
+
+    it("At the start monthClaimed is 0 for both nfts", async () => {
+      await testMonthClaimed(reward, [nftId1, nftId2], [0, 0]);
+    });
+
+    it("When a nft claims in month 0, month claimed still stays 0", async () => {
+      await reward.connect(nftMinter1).claim(nftId1);
+      await testMonthClaimed(reward, [nftId1, nftId2], [0, 0]);
+    });
+    it("In month 1, when you nftId1 claims it updates month last claimed to 1", async () => {
+      await mineDays(30, network);
+      await reward.connect(nftMinter1).claim(nftId1);
+      await testMonthClaimed(reward, [nftId1, nftId2], [1, 0]);
+    });
+
+    it("In month 1, 5 days later it when nft Id 1 claims again it stays at id 1", async () => {
+      await mineDays(5, network);
+      await reward.connect(nftMinter1).claim(nftId1);
+      await testMonthClaimed(reward, [nftId1, nftId2], [1, 0]);
+    });
+
+    it("In month 2, nft Id 2 claims and monthClaimed for nft Id 2 becomes 2", async () => {
+      await mineDays(30, network);
+      await reward.connect(nftMinter2).claim(nftId2);
+      await testMonthClaimed(reward, [nftId1, nftId2], [1, 2]);
+    });
+
+    it("In month 3, nft Id 1 and 2 claim again and monthClaimed for both become 3", async () => {
+      await mineDays(30, network);
+      await reward.connect(nftMinter1).claim(nftId1);
+      await reward.connect(nftMinter2).claim(nftId2);
+      await testMonthClaimed(reward, [nftId1, nftId2], [3, 3]);
+    });
+
+    it("Going to later months does not change anything", async () => {
+      await mineDays(30, network);
+      await testMonthClaimed(reward, [nftId1, nftId2], [3, 3]);
+      await mineDays(30, network);
+      await testMonthClaimed(reward, [nftId1, nftId2], [3, 3]);
+      await mineDays(30, network);
+      await testMonthClaimed(reward, [nftId1, nftId2], [3, 3]);
+      await mineDays(30, network);
+      await testMonthClaimed(reward, [nftId1, nftId2], [3, 3]);
+      await mineDays(30, network);
+      await testMonthClaimed(reward, [nftId1, nftId2], [3, 3]);
     });
   });
 });
